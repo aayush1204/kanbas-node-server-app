4b3561e6afa8ff603d271f27159d7a90b4af72a2
Aayush Work <aayush@catalan.ai>
2024-04-18 22:37:35 -0400

    Final

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import express from 'express';
+import "dotenv/config";
 import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
+
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+   );
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));
+     
 app.use(express.json());
 
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
